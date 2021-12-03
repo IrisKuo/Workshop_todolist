75f5b46faffc3cdafc3b20f347cb2b837f5c69c1
Iris Kuo <iriskuo0523@gmail.com>
2021-12-03 12:47:13 +0800

    task_1

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,13 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", function (){
+
+  const selectAll = document.querySelectorAll('li');
+  selectAll.forEach(li => li.addEventListener('click',function(){
+    console.log(li)
+    if (li.className !== "checked" ){
+     li.className = "checked"}
+    else{li.className = ""}
+
+  })
+  )
+});
