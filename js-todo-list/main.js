c514f8c22dc9b923c7f1b52f2b94e71dc56e360b
Iris Kuo <iriskuo0523@gmail.com>
2021-12-03 14:35:31 +0800

    task_2

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -3,11 +3,19 @@ document.addEventListener("DOMContentLoaded", function (){
 
   const selectAll = document.querySelectorAll('li');
   selectAll.forEach(li => li.addEventListener('click',function(){
-    console.log(li)
+    // console.log(li)
     if (li.className !== "checked" ){
      li.className = "checked"}
     else{li.className = ""}
 
   })
   )
+  const closeBtn = document.querySelector(".container")
+  
+  closeBtn.addEventListener('click',function(e){
+    console.log(e)
+    if(e.target.innerText == 'x'){
+    e.target.parentNode.remove();}
+  });
+
 });
